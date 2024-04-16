53e8b6902f29a83210f0086e9e1404f4c660a7eb
lucatelli <gefersonlucatelli@gmail.com>
2024-04-16 22:35:18 +0100

    Fix casatasks issues; finish bandpass and gaincal implementation (test).

--- main.py
+++ main.py
@@ -2,33 +2,43 @@ import os, glob, re, logging
 from sys import argv
 import time, argparse
 from datetime import datetime
-import casatasks, casatools
-import casalogger
-import casaplotms
 import numpy as np
 import subprocess
 import matplotlib
 import configparser
 import matplotlib.pyplot as plt
 
-msmd = casatools.msmetadata()
-ms = casatools.ms()
-tb = casatools.table()
+
+try:
+    import casatasks, casatools, casaplotms
+    from casaplotms import plotms
+    from casatasks import gaincal, gencal, applycal, bandpass, split, importasdm
+    from casatasks import flagdata, flagmanager, plotants, plotweather, hanningsmooth
+    from casatasks import clearcal, delmod, setjy, fluxscale, tclean, imhead, listobs
+    import casalogger
+    msmd = casatools.msmetadata()
+    ms = casatools.ms()
+    tb = casatools.table()
+except Exception as e:
+    print(f"Error {e} while importing casa utilities."
+          f"Are you inside a CASA environment?")
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+print("Creating log dir")
+log_dir = os.path.join(os.getcwd(), 'logs')
+
+if not os.path.exists(log_dir):
+    print(f"Log directory '{log_dir}' does not exist, creating it...")
+    os.makedirs(log_dir, exist_ok=True)
+else:
+    print(f"Log directory '{log_dir}' exists")
 
 try:
-    print("Creating log dir")
-    log_dir = os.path.join(os.getcwd(), 'logs')
-    
-    if not os.path.exists(log_dir):
-        print(f"Log directory '{log_dir}' does not exist, creating it...")
-        os.makedirs(log_dir, exist_ok=True)
-    else:
-        print(f"Log directory '{log_dir}' exists")
-
+    console
+except:
     logfile_name = datetime.now().strftime('jvla_pipe_%H:%M:%S_%d:%m:%Y.log')
     filename = os.path.join(log_dir, logfile_name)
     
@@ -38,13 +48,11 @@ try:
     logging.getLogger('').addHandler(console)
     logging.getLogger('matplotlib').setLevel(logging.WARNING)
 
-except Exception as e:
-    print(f"Exception {e} occurred")
 
 
-exec(open("./data_reduction/helper_functions.py").read())
-exec(open("./data_reduction/flagging.py").read())
-exec(open('./data_reduction/calibrate.py').read())
+exec(open("./data_calibration/helper_functions.py").read())
+exec(open("./data_calibration/flagging.py").read())
+exec(open('./data_calibration/calibrate.py').read())
 
 # loading data
 load_data = config.getboolean('globals','load_data')
@@ -72,6 +80,8 @@ use_aoflagger = config.getboolean('flagging','use_aoflagger')
 aoflagger_sif = config.get('flagging','aoflagger_sif')
 aoflagger_strategy = config.get('flagging','aoflagger_strategy')
 edge_channel_frac = config.getfloat('flagging','edge_channel_frac')
+edge_channel_flag_frac = config.getfloat('flagging','edge_channel_flag_frac')
+do_flag_edge_channels = config.getboolean('flagging','do_flag_edge_channels')
 
 # average
 do_average = config.getboolean('average','do_average')
@@ -94,11 +104,16 @@ bp_solint_G_ap = config.get('calibrate','bp_solint_G_ap')
 bp_solint_BP = config.get('calibrate','bp_solint_BP')
 minsnr = config.getfloat('calibrate','minsnr')
 do_bandpass_1st_run = config.getboolean('calibrate','do_bandpass_1st_run')
-do_all_phases = config.getboolean('calibrate','do_all_phases')
+do_gain_calibration_1st_run = config.getboolean('calibrate','do_gain_calibration_1st_run')
+do_bandpass_2nd_run = config.getboolean('calibrate','do_bandpass_2nd_run')
+do_gain_calibration_2nd_run = config.getboolean('calibrate','do_gain_calibration_2nd_run')
+
 all_solint_short_p = config.get('calibrate','all_solint_short_p')
 all_solint_short_ap = config.get('calibrate','all_solint_short_ap')
 all_solint_long_p = config.get('calibrate','all_solint_long_p')
 all_solint_inf_ap = config.get('calibrate','all_solint_inf_ap')
+
+do_apply_science = config.getboolean('calibrate','do_apply_science')
 
 # defining vis here
 
@@ -127,12 +142,12 @@ except Exception as e:
 if load_data == True and 'load_data' not in steps_performed:
     try:
         logging.info("Running CASA task importasdm")
-        vis_for_cal = importasdm()
+        vis_for_cal = import_data()
         # getfields()
+        steps_performed.append('load_data')
     except Exception as e:
         logging.critical(f"Exception {e} occured")
-
-    steps_performed.append('load_data')
+        
 
 if ms_info == True:
     try:
@@ -204,6 +219,65 @@ if do_bandpass_1st_run == True and 'bandpass_1st' not in steps_performed:
         logging.info("Running bandpass calibration")
         (gaintables_apply_BP_1, gainfield_bandpass_apply_1, gain_tables_BP_dict_1,
          gaintables_apply_BP_dict_1,gainfields_apply_BP_dict_1) = bandpass_cal(i=1)
-        steps_performed.append('bandpass')
+        steps_performed.append('bandpass_1st')
     except Exception as e:
         logging.critical(f"Exception {e} while running bandpass calibration")
+
+if do_gain_calibration_1st_run == True and 'gain_calibration_1st' not in steps_performed:
+    try:
+        logging.info("Running gain calibration")
+        (gain_tables_phases_dict_1,
+         gain_tables_ampphase_for_science_1,gain_tables_to_apply_science_1,
+         flag_FLUX_SCALE_1) = cal_phases_amplitudes(gaintables_apply_BP_1,
+                                                    gainfield_bandpass_apply_1,
+                                                    i=1)
+        run_rflag(vis=vis_for_cal,i=1,field=calibrators_all)
+
+        make_plots_stages(vis=vis_for_cal,
+                          stage='after',
+                          kind='after_apply_calibration_and_flag_iter_1',
+                          FIELDS=calibrators_all_arr)
+
+        steps_performed.append('gain_calibration_1st')
+    except Exception as e:
+        logging.critical(f"Exception {e} while running gain calibration")
+
+
+if do_bandpass_2nd_run == True and 'bandpass_2nd' not in steps_performed:
+    try:
+        logging.info("Running bandpass calibration")
+        (gaintables_apply_BP_2, gainfield_bandpass_apply_2, gain_tables_BP_dict_2,
+         gaintables_apply_BP_dict_2,gainfields_apply_BP_dict_2) = bandpass_cal(i=2)
+        steps_performed.append('bandpass_2nd')
+    except Exception as e:
+        logging.critical(f"Exception {e} while running bandpass calibration")
+
+
+if do_gain_calibration_2nd_run == True and 'gain_calibration_2nd' not in steps_performed:
+    try:
+        logging.info("Running gain calibration")
+        (gain_tables_phases_dict_1,
+         gain_tables_ampphase_for_science_2,gain_tables_to_apply_science_2,
+         flag_FLUX_SCALE_2) = cal_phases_amplitudes(gaintables_apply_BP_2,
+                                                    gainfield_bandpass_apply_2,
+                                                    i=2)
+        run_rflag(vis=vis_for_cal,i=2,field=calibrators_all)
+
+        make_plots_stages(vis=vis_for_cal,
+                          stage='after',
+                          kind='after_apply_calibration_and_flag_iter_2',
+                          FIELDS=calibrators_all_arr)
+
+        steps_performed.append('gain_calibration_2nd')
+    except Exception as e:
+        logging.critical(f"Exception {e} while running gain calibration")
+
+
+# if do_apply_science == True and 'apply_science' not in steps_performed:
+#     try:
+#         logging.info("Applying calibration to science target")
+#         _ = apply_cal_to_science(init_tables, gaintables_apply_BP_2,
+#                              gain_tables_ampphase_for_science_2)
+#         steps_performed.append('apply_science')
+#     except Exception as e:
+#         logging.critical(f"Exception {e} while applying calibration to science.")
