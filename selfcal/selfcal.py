80e04fa0eff7dd82ca8c35074c7cc19d1b8e6b60
kelvinwandia <muriithikangeri@gmail.com>
2024-04-23 14:45:35 +0100

    Selfcal file -- somewhat working csv writer

--- selfcal/selfcal.py
+++ selfcal/selfcal.py
@@ -86,7 +86,8 @@ def make_bdsf_catalogue(input_image, detection_threshold):
 
     ## thresh=hard -- hard thresh is assumed given by thresh_pix
     ## thresh_isl -- determines the region to which the fitting is done
-    img = bdsf.process_image(input_image,adaptive_rms_box=True, thresh='hard', thresh_isl=True, thresh_pix = detection_threshold)
+    ## instead of using adaptive boxes -- use spline interpolation which is set using sline rank -- its much better
+    img = bdsf.process_image(input_image,adaptive_rms_box=False, spline_rank=4, thresh='hard', thresh_isl=True, thresh_pix = detection_threshold)
 
     # write the source catalog list
 
@@ -94,14 +95,54 @@ def make_bdsf_catalogue(input_image, detection_threshold):
         img.write_catalog(format='csv',catalog_type='srl',clobber=True)
 
     # write the residual image
-    img.export_image(outfile=input_image.replace('.image','_pybdsf.fits'),img_type='gaus_resid',clobber=True)
+    img.export_image(outfile=input_image.replace('.fits','_gauss.residual.fits'),img_type='gaus_resid',clobber=True)
 
     # write the model image
-    img.export_image(outfile=input_image.replace('.image','_gaus.model.fits'),img_type='gaus_model',clobber=True)
+    img.export_image(outfile=input_image.replace('.fits','_gaus.model.fits'),img_type='gaus_model',clobber=True)
 
     # write the rms image
-    img.export_image(outfile=input_image.replace('.image','_rms.fits'),img_type='rms',clobber=True)
+    img.export_image(outfile=input_image.replace('.fits','_rms.fits'),img_type='rms',clobber=True)
 
 
-input_image = '/home/kelvin/Desktop/vla/working_directory/selfcal/obj_prelim_clean.3arcmin.pb.tt0'
-make_bdsf_catalogue(input_image,detection_threshold=5.0)
+input_image = '/home/kelvin/Desktop/vla/working_directory/selfcal/obj_prelim_clean.fits'
+# make_bdsf_catalogue(input_image,detection_threshold=5.0)
+
+import csv
+
+# Define input and output file paths
+
+
+def write_to_csv(input_file):
+
+    output_file = input_file.replace('.srl','.csv')
+    with open(input_file, "r") as infile, open(output_file, "w", newline="") as outfile:
+        # Create a CSV writer object
+        writer = csv.writer(outfile)
+        
+        # Write the header row
+        writer.writerow(["Source_id", "Isl_id", "RA", "E_RA", "DEC", "E_DEC", "Total_flux", "E_Total_flux",
+                        "Peak_flux", "E_Peak_flux", "RA_max", "E_RA_max", "DEC_max", "E_DEC_max", "Maj", "E_Maj",
+                        "Min", "E_Min", "PA", "E_PA", "Maj_img_plane", "E_Maj_img_plane", "Min_img_plane",
+                        "E_Min_img_plane", "PA_img_plane", "E_PA_img_plane", "DC_Maj", "E_DC_Maj", "DC_Min",
+                        "E_DC_Min", "DC_PA", "E_DC_PA", "DC_Maj_img_plane", "E_DC_Maj_img_plane", "DC_Min_img_plane",
+                        "E_DC_Min_img_plane", "DC_PA_img_plane", "E_DC_PA_img_plane", "Isl_Total_flux",
+                        "E_Isl_Total_flux", "Isl_rms", "Isl_mean", "Resid_Isl_rms", "Resid_Isl_mean", "S_Code"])
+        
+        # Skip the first few lines of comments
+        for _ in range(5):
+            next(infile)
+        
+        # Read each line of the input file
+        for line in infile:
+            # Skip empty lines
+            if not line.strip():
+                continue
+            
+            # Split the line into fields
+            fields = line.split(",")
+            
+            # Write the fields to the CSV file
+            writer.writerow([field.strip() for field in fields])
+            
+input_file = "/home/kelvin/Desktop/vla/working_directory/selfcal/obj_prelim_clean.pybdsf.srl"
+write_to_csv(input_file)
